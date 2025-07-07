876489d578579e0d93ad0f35b67bb1e4220fd375
rawa08 <rawa_aref@hotmail.com>
2025-07-07 14:24:17 +0200

    feat(admin-routes): mount venueAdminRouter on '/venues' with requireAdmin middleware

--- src/routes/dashboardRoutes/admin/index.ts
+++ src/routes/dashboardRoutes/admin/index.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import userAdminRouter from './user';
+import venueAdminRouter from './venues';
+import { requireAdmin } from '@/middleware/authMiddleware';
 
 const adminRouter = Router();
 
 adminRouter.use('/user', userAdminRouter);
+adminRouter.use('/venues', requireAdmin, venueAdminRouter);
 
 export default adminRouter
