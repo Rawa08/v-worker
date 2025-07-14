78f5750d2885a7e51081536a9afd4e0e8c996f97
rawa08 <rawa_aref@hotmail.com>
2025-07-14 20:25:44 +0200

    Add getDeviceByAndroidId and createNewDevice utility functions to device service

--- src/services/repos/device.service.ts
+++ src/services/repos/device.service.ts
@@ -1,11 +1,21 @@
 import { prisma } from "@/services/prismaService";
-import type { CreateDeviceInput, UpdateDeviceInput } from '@/models/device.schema';
+import type { Device } from '@/generated/prisma'
+import type { UpdateDeviceInput } from '@/models/device.schema';
+
+const getDeviceByAndroidId = async (androidId: string): Promise<Device | null> => {
+  const device = await prisma.device.findUnique({ where: { androidId } });
+  return device;
+};
+
+const createNewDevice = async (androidId: string | undefined): Promise<Device> => {
+  const device = await prisma.device.create({ data: androidId ? { androidId } : {} });
+  return device;
+};
 
 const getAllDevices = () => prisma.device.findMany();
 const getDeviceById = (id: string) => prisma.device.findUnique({ where: { id } });
-const createDevice = (data: CreateDeviceInput) => prisma.device.create({ data });
 const updateDevice = (id: string, data: UpdateDeviceInput) =>
   prisma.device.update({ where: { id }, data });
 const deleteDevice = (id: string) => prisma.device.delete({ where: { id } });
 
-export { getAllDevices, getDeviceById, createDevice, updateDevice, deleteDevice };
+export { getAllDevices, getDeviceById, createNewDevice, updateDevice, deleteDevice, getDeviceByAndroidId };
